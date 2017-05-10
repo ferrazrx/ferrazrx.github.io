ff622cf23ac4608e3f0759451fca7af19c85eca4
rafael.ferra <ferrazrx@gmail.com>
2017-05-10 19:46:04 -0300

    XLMHttpRequest

--- topics/js/topic4.js
+++ topics/js/topic4.js
@@ -35,12 +35,12 @@ function loadGET (url, callback){
 }
 
 function ajax(){
-  document.querySelector('.fa').style.display = 'inline-block';
+  document.querySelector('.spinner').style.display = 'inline-block';
   document.querySelector('#button').style.display = 'none';
   //https://jsonplaceholder.typicode.com/users
   loadGET('https://jsonplaceholder.typicode.com/users', function(res){
     localStorage.setItem('users', res);
-    document.querySelector('.fa').style.display = 'none';
+    document.querySelector('.spinner').style.display = 'none';
 
     var string = localStorage.getItem('users');
     var users = JSON.parse(string);
@@ -62,6 +62,7 @@ function ajax(){
       var b = document.createElement('div');
       b.setAttribute('class', 'panel-body');
       b.setAttribute('id', user.id);
+
       b.innerHTML = user.website;
       b.style.display = 'none';
       b.style.transition = 'display .7s easy-in';
@@ -72,3 +73,62 @@ function ajax(){
     });
   });
 };
+
+//Exemple 2
+
+function request(url, callback){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200 ){
+      console.log(this);
+      callback(this.responseText);
+    }
+  }
+
+  request.open(
+    'GET',
+    url,
+    true
+  );
+  request.send();
+}
+
+function table(){
+  document.querySelector('.spinner1').style.display = 'inline-block';
+  document.querySelector('#button1').style.display = 'none';
+  request('https://jsonplaceholder.typicode.com/albums',
+    function(response){
+        var table = document.createElement('table');
+        table.setAttribute('class', 'table');
+        var tr = document.createElement('tr');
+        var th1 = document.createElement('th');
+        var th2 = document.createElement('th');
+        var th3 = document.createElement('th');
+        th1.innerHTML = 'id';
+        th2.innerHTML = 'UserID';
+        th3.innerHTML = 'Title';
+        table.appendChild(tr);
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
+        var obj = JSON.parse(response);
+        obj.forEach(function(item){
+            var tr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            var td2 = document.createElement('td');
+            var td3 = document.createElement('td');
+            td1.innerHTML = item.id;
+            td2.innerHTML = item.userId;
+            td3.innerHTML = item.title;
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            table.appendChild(tr);
+        })
+        document.getElementById('rows').setAttribute('class', 'panel-body');
+        document.getElementById('rows').appendChild(table);
+        document.getElementById('table').style.display = 'block';
+        document.querySelector('.spinner1').style.display = 'none';
+    }
+  );
+}
