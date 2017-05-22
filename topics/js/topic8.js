014b7987ec810e0aa9ccf9b2e61b1cdb2034169f
rafael.ferra <ferrazrx@gmail.com>
2017-05-22 13:20:19 -0300

    Week 5

---
+++ topics/js/topic8.js
@@ -0,0 +1,39 @@
+document.getElementById('button').addEventListener('click', createCloud);
+document.getElementById('button2').addEventListener('click', createSun);
+document.getElementById('button3').addEventListener('click', createHelicopter);
+
+
+function createCloud(){
+  var y = Math.abs(Math.random()*300);
+  var x = Math.abs(Math.random()*700);
+
+
+  console.log(x+","+y);
+  var sky = document.getElementById('sky');
+  var cloud = document.createElement('img');
+  cloud.src = "img/bigCloud.png";
+  cloud.setAttribute('class', 'cloud');
+  cloud.setAttribute('style', 'top:'+y+"px;"+"left:"+x+"px;");
+  sky.appendChild(cloud);
+
+}
+
+function createSun(){
+  var sky = document.getElementById('sky');
+  var sun = document.createElement('img');
+  sun.src = "img/sun.png";
+  sun.setAttribute('class', 'sun');
+  sky.appendChild(sun);
+  document.getElementById('button2').style.display = 'none';
+
+}
+
+function createHelicopter(){
+  var sky = document.getElementById('sky');
+  var sun = document.createElement('img');
+  sun.src = "img/helicopter.gif";
+  sun.setAttribute('class', 'helicopter');
+  sky.appendChild(sun);
+  document.getElementById('button3').style.display = 'none';
+
+}
