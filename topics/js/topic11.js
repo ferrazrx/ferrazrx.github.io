2471f4f6f8b5faec71e0790af719e53b44e0f5b1
rafael.ferra <ferrazrx@gmail.com>
2017-05-24 10:31:24 -0300

    Week 5

--- topics/js/topic11.js
+++ topics/js/topic11.js
@@ -1,9 +1,4 @@
 window.onLoad() = function (){
   var spinner = document.getElementsByClassName("se-pre-con")[0];
-  spinner.style.display = 'block';
-}
-
-function onLoad(){
-  var spinner = document.getElementsByClassName("se-pre-con")[0];
   spinner.style.display = 'none';
 }
