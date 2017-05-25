c0e4fa780ee8111d97bb0a75e8e275d85f195e44
rafael.ferra <ferrazrx@gmail.com>
2017-05-25 11:56:06 -0300

    Week 6

--- topics/js/topic9.js
+++ topics/js/topic9.js
@@ -7,9 +7,11 @@ window.addEventListener('load', function(){
     touchobj = null; // Touch object holder
 
     box2.addEventListener('touchstart', function(e){
+        var info = document.getElementById('box');
         touchobj = e.changedTouches[0]; // reference first touch point
         boxleft = parseInt(box2.style.left); // get left position of box
         startx = parseInt(touchobj.clientX); // get x coord of touch point
+        info.innerHTML = touchobj + '<br>' + boxleft + '<br>' + startx;
         e.preventDefault(); // prevent default click behavior
     }, false)
 
@@ -18,7 +20,7 @@ window.addEventListener('load', function(){
         var dist = parseInt(touchobj.clientX) - startx; // calculate dist traveled by touch point
         // move box according to starting pos plus dist
         // with lower limit 0 and upper limit 380 so it doesn't move outside track:
-        box2.style.left = ( (boxleft + dist > 380)? 380 : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
+        box2.style.left = ( (boxleft + dist > 240)? 240 : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
         e.preventDefault();
     }, false)
 
