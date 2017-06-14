e4d5a60bf9e0ad2d5cdd7f7237d1b5a217b1b51a
rafael.ferra <ferrazrx@gmail.com>
2017-06-13 23:38:31 -0300

    Week 9

--- topics/js/topic13.js
+++ topics/js/topic13.js
@@ -17,3 +17,12 @@ function close(){
   menu.classList="";
   menu.classList.add('close');
 }
+
+document.getElementById('more').addEventListener('click', open);
+
+function open(){
+  var info = document.getElementById('info');
+  info.classList.toggle('closed');
+  info.classList.toggle('open');
+
+}
