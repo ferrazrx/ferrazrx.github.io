0d4a601fc0c36f66d48220307b47ca40024c1fd0
rafael.ferra <ferrazrx@gmail.com>
2017-07-11 21:21:49 -0300

    Week 13

--- Project/js/controller/index.addmedicine.js
+++ Project/js/controller/index.addmedicine.js
@@ -3,8 +3,8 @@ function addMedicine(){
 	var amount = document.getElementById("amount").value;
 	var days = document.getElementsByClassName("day");
 	var customer = JSON.parse(sessionStorage.getItem("actualCustomer"));
-	
-	
+
+
 	if(medicine == "" || amount == "" || days == "" ){
 		errorMessage("All filds must be complete! Please don't left any black space.");
 	}else{
@@ -13,17 +13,18 @@ function addMedicine(){
 			if(days[i].selected == true){
 				week.push(days[i].value);
 			}
-		};	
+		};
 		var medicine = new Medicine(medicine,amount, week);
 		customer.medicines.push(medicine);
-		var storage = customer.email;
-		updateStorage(storage, customer);
-		goToApp();
+		updateStorage(customer);
+
 	}
 }
 
-function updateStorage(name, obj){
+function updateStorage(obj){
+	var pacient = obj.email;
 	var string = JSON.stringify(obj);
-	localStorage.setItem(name, string);
+	localStorage.setItem(pacient, string);
 	sessionStorage.setItem("actualCustomer", string);
+	goToApp();
 }
--- Project/js/view/index.app.js
+++ Project/js/view/index.app.js
@@ -7,21 +7,21 @@ function openApp(customerExists) {
 
 function goToApp(){
 	var pacient = JSON.parse(sessionStorage.getItem("actualCustomer"));
-	
+
   var app = document.getElementsByClassName("welcome")[0];
   app.innerHTML = "";
-  
+
   var medicineForm = document.getElementsByClassName("medicineForm")[0];
   medicineForm.style.display = "flex";
-  
+
   var name = document.getElementById("pacient");
   var age = document.getElementById("year");
   var medications = document.getElementById("medications");
-  
+
   name.innerHTML = "<strong>Name: </strong>" + pacient.name;
   age.innerHTML = "<strong>Age: </strong>" + pacient.age;
   medications.innerHTML =  "<strong>Medications: </strong>" + pacient.medicines.length;
-  
+
   var sunday = document.getElementById("Sunday").innerHTML = "Pills:";
   var monday = document.getElementById("Monday").innerHTML = "Pills:";
   var tuesday = document.getElementById("Tuesday").innerHTML = "Pills:";
@@ -29,7 +29,7 @@ function goToApp(){
   var thursday = document.getElementById("Thursday").innerHTML = "Pills:";
   var friday = document.getElementById("Friday").innerHTML = "Pills:";
   var saturday = document.getElementById("Saturday").innerHTML = "Pills:";
-  
+
   if(pacient.medicines.length>0){
 	for(var item in pacient.medicines){
 		var name = pacient.medicines[item].name;
@@ -38,24 +38,40 @@ function goToApp(){
 		for(var day in days){
 			var weekDay = document.getElementById(days[day]);
 			var div = document.createElement("div");
-			var img = document.createElement("img");
+			var img1 = document.createElement("img");
+      var img2 = document.createElement("img");
 			var p1 = document.createElement("p");
 			var p2 = document.createElement("p");
-			
-			img.src = "img/pill1.png";
-			img.alt = "pillIgm";
-			img.style.width = "20px";
-			
+
+			img1.src = "img/pill1.png";
+			img1.alt = "pillIgm";
+			img1.style.width = "20px";
+
+      img2.src = "img/close-icon.png";
+      img2.alt = "close";
+      img2.setAttribute("id",item);
+      img2.style.width = "15px";
+      img2.style.cursor="pointer";
+      img2.setAttribute("onclick","remove(this)");
+
 			p1.innerHTML = "Name: " + name;
 			p2.innerHTML = "Amount: " + amount;
-			
+
 			div.classList.add("transparent");
-			div.appendChild(img);
+			div.appendChild(img1);
+      div.appendChild(img2);
 			div.appendChild(p1);
 			div.appendChild(p2);
-	
+
 			weekDay.appendChild(div);
 		}
 	}
-  }
+}
+}
+
+function remove(id){
+  console.log(id.id);
+  var pacient = JSON.parse(sessionStorage.getItem("actualCustomer"));
+  pacient.medicines.splice(id,1);
+  updateStorage(pacient);
 }
