16a353fc74154a054af0d1d8019d412e7f0bb083
nscc-halifax <ferrazrx@gmail.com>
2018-12-01 20:40:25 -0400

    New Page Design

---
+++ js/index.js
@@ -0,0 +1,65 @@
+class Typing {
+  //Constructor
+  constructor(array) {
+    this.words = array;
+    this.state_direction = 'forward';
+    this.state_word = 0;
+    this.state_caracter = 0;
+  }
+  //Animate function
+  animateWords() {
+    if (this.state_direction == 'forward') {
+      let letter = this.words[this.state_word].charAt(this.state_caracter);
+      document.querySelector('.dinamic-words').innerHTML += letter;
+      this.state_caracter++;
+
+      if (this.state_caracter == this.words[this.state_word].length) {
+        this.state_direction = 'backword';
+      }
+    }
+    if (this.state_direction == 'backword') {
+      let word = this.words[this.state_word];
+      let sub_word = word.substr(0, this.state_caracter);
+      this.state_caracter--;
+      document.querySelector('.dinamic-words').innerHTML = sub_word;
+
+      if (this.state_caracter == -1) {
+        this.state_word++;
+        this.state_direction = 'forward';
+      }
+    }
+    if (this.state_word == this.words.length) this.state_word = 0;
+  }
+
+  animate() {
+    setInterval(this.animateWords.bind(this), 120);
+  }
+}
+
+window.onload = ()=>{
+  const typing = new Typing(['Developer. ', 'Designer. ', 'Programmer. ']);
+  typing.animate();
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  window.onscroll = function () { 
+    var nav = document.getElementById('nav');
+    
+    if (window.pageYOffset >= 150 ) {
+      nav.classList.add("nav--orange");
+      nav.classList.remove("nav--transparent");
+    } 
+    else {
+      nav.classList.add("nav--transparent");
+      nav.classList.remove("nav--orange");
+    }
+  }
+}
+
